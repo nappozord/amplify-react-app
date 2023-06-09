02b90451bd799e9bdbb9c30aef25e6365e1c547b
Alessandro Napoletano <38859529+nappozord@users.noreply.github.com>
2023-06-09 23:33:23 +0200

    initial-commit

---
+++ amplify-react/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api6bab9d0b": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "amplifyreactlambda123": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
